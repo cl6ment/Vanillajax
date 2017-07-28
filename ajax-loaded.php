05e593c722e6a50b543e3935e1bb2c1fe46db7dc
Clément H <clement.hve@gmail.com>
2017-07-28 17:53:26 +0200

    Update and rename ajax-loaded.php to ajax.php

--- ajax-loaded.php
+++
@@ -1,3 +0,0 @@
-<p>Salut <?php echo (!empty($_GET["name"]))?$_GET["name"]:"inconnu"; ?> !</p>
-
-<p>Tu as <?php echo (!empty($_GET["age"]))?$_GET["age"]:"..."; ?> ans</p>
---
+++ ajax.php
@@ -0,0 +1,2 @@
+<?php echo (!empty($_GET["name"]))?$_GET["name"]:""; ?>
+<?php echo (!empty($_GET["age"]))?$_GET["age"]:""; ?>
